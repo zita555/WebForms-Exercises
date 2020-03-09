399e73212dfbee63622531e5164f1d8f76a68fd3
Shingai Zindi <szindi@users.noreply.github.com>
2020-03-09 11:13:05 -0600

    add entites

---
+++ FSISSystem/ENTITIES/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSISSystem.ENTITIES
+{
+    class Player
+    {
+        public int PlayerID { get; set; }
+        public int GuardianID { get; set; }
+        public int TeamID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public char Gender { get; set; }
+        public string AlbertaHealthCaseNumber { get; set; }
+        public string MedicalAlertDetails { get; set; }
+
+    }
+}
---
+++ FSISSystem/ENTITIES/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSISSystem.ENTITIES
+{
+    class Team
+    {
+        public int TeamID { get; set; }
+        public string TeamName { get; set; }
+        public string Coach { get; set; }
+        public string AssistantCoach { get; set; }
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+    }
+}
